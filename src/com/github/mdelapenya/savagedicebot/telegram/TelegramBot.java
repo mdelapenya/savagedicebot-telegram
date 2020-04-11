bb6d8a2441b1cde5e06310e4e0d0f6962dba943a
Manuel de la Peña <mdelapenya@gmail.com>
2020-04-11 15:58:35 +0200

    chore: replace anonymous Threads with lambdas

--- src/com/github/mdelapenya/savagedicebot/telegram/TelegramBot.java
+++ src/com/github/mdelapenya/savagedicebot/telegram/TelegramBot.java
@@ -58,59 +58,55 @@ public abstract class TelegramBot extends TelegramLongPollingBot {
 
     @Override
     public final void onUpdateReceived(final Update update) {
-        Thread thread = new Thread() {
+        Thread thread = new Thread(() -> {
+            try {
+                TelegramBotMessage message = new TelegramBotMessage(
+                    update.getMessage().getFrom().getId(),
+                    update.getMessage().getChatId(),
+                    update.getMessage().getFrom().getFirstName(),
+                    update.getMessage().getFrom().getLastName(),
+                    update.getMessage().getFrom().getUserName()
+                );
 
-            @Override
-            public void run() {
-                try {
-                    TelegramBotMessage message = new TelegramBotMessage(
-                        update.getMessage().getFrom().getId(),
-                        update.getMessage().getChatId(),
-                        update.getMessage().getFrom().getFirstName(),
-                        update.getMessage().getFrom().getLastName(),
-                        update.getMessage().getFrom().getUserName()
-                    );
-                    
-                    if(TelegramBot.this.telegramBotListener!= null && update.hasMessage()) {
-                        if(update.getMessage().hasText()) {
-                            message.setMessageText(update.getMessage().getText());
-                            
-                            if(message.getMessageText().startsWith("/")) {
-                                String data[] = message.getMessageText().split(" ");
-                                String cmd = null;
-                                ArrayList<String> params = new ArrayList<>();
+                if(TelegramBot.this.telegramBotListener!= null && update.hasMessage()) {
+                    if(update.getMessage().hasText()) {
+                        message.setMessageText(update.getMessage().getText());
 
-                                for(String dt : data) {
-                                    if(cmd == null) {
-                                        cmd = dt;
-                                    } else {
-                                        params.add(dt);
-                                    }
+                        if(message.getMessageText().startsWith("/")) {
+                            String data[] = message.getMessageText().split(" ");
+                            String cmd = null;
+                            ArrayList<String> params = new ArrayList<>();
+
+                            for(String dt : data) {
+                                if(cmd == null) {
+                                    cmd = dt;
+                                } else {
+                                    params.add(dt);
                                 }
-
-                                message.setCommand(cmd);
-                                message.setParams(params);
                             }
+
+                            message.setCommand(cmd);
+                            message.setParams(params);
                         }
-                        
-                        if(TelegramBot.this.allowImages && update.getMessage().hasPhoto()) {
-                            message.setFiles(TelegramFile.getPhotos(TelegramBot.this, update));
-                        }
-                        
-                        if(TelegramBot.this.allowDocuments && update.getMessage().hasDocument()) {
-                            ArrayList<TelegramFile> files = new ArrayList<>();
-                            files.add(TelegramFile.getDocument(TelegramBot.this, update));
-                            message.setFiles(files);
-                        }
-                        
-                        TelegramBot.this.telegramBotListener.receiveMessage(message);
                     }
-                }
-                catch (Exception e) {
-                    e.printStackTrace();
+
+                    if(TelegramBot.this.allowImages && update.getMessage().hasPhoto()) {
+                        message.setFiles(TelegramFile.getPhotos(TelegramBot.this, update));
+                    }
+
+                    if(TelegramBot.this.allowDocuments && update.getMessage().hasDocument()) {
+                        ArrayList<TelegramFile> files = new ArrayList<>();
+                        files.add(TelegramFile.getDocument(TelegramBot.this, update));
+                        message.setFiles(files);
+                    }
+
+                    TelegramBot.this.telegramBotListener.receiveMessage(message);
                 }
             }
-        };
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
         
         thread.start();
     }
@@ -121,17 +117,13 @@ public abstract class TelegramBot extends TelegramLongPollingBot {
     }
     
     public final void sendText(final long chatId, final String text) {
-        Thread thread = new Thread() {
-
-            @Override
-            public void run() {
-                try {
-                    SendMessage message = new SendMessage().setParseMode("HTML").setChatId(chatId).setText(text);
-                    TelegramBot.this.execute(message);
-                } catch (Exception e) {
-                }
+        Thread thread = new Thread(() -> {
+            try {
+                SendMessage message = new SendMessage().setParseMode("HTML").setChatId(chatId).setText(text);
+                TelegramBot.this.execute(message);
+            } catch (Exception e) {
             }
-        };
+        });
         
         thread.start();
     }
@@ -141,54 +133,50 @@ public abstract class TelegramBot extends TelegramLongPollingBot {
     }
     
     public final void sendImage(final long chatId, final Object imagePath, final String text) {
-        Thread thread = new Thread() {
+        Thread thread = new Thread(() -> {
+            try {
+                Object img = imagePath;
 
-            @Override
-            public void run() {
-                try {
-                    Object img = imagePath;
-                    
-                    File imgPath = null;
-                    
-                    if(img instanceof String) {
-                        String path = (String)img;
-                        
-                        if(path.toLowerCase().startsWith("http")) {
-                            img = new URL(path);
-                        } else {
-                            img = new File((String) img);
-                        }
+                File imgPath = null;
+
+                if(img instanceof String) {
+                    String path = (String)img;
+
+                    if(path.toLowerCase().startsWith("http")) {
+                        img = new URL(path);
+                    } else {
+                        img = new File((String) img);
                     }
-                    
-                    if(img instanceof File) {
-                        imgPath = (File)img;
-                    } else if(img instanceof URL) {
-                        URL url = (URL)img;
-                        
-                        String tempFile = TelegramBot.TEMP_DIR + "/" + Utilities.timeStamp();
-                        
-                        try(FileOutputStream fos = new FileOutputStream(tempFile)) {
-                            fos.write(Utilities.getURL(url.toString()));
-                        }
-                        
-                        imgPath = new File(tempFile);
-                    }
-                    else {
-                        return;
-                    }
-                    
-                    
-                    SendPhoto photo = new SendPhoto().setChatId(chatId).setNewPhoto(imgPath);
-                    
-                    if(text != null) {
-                        photo.setCaption(text);
-                    }
-                    
-                    TelegramBot.this.sendPhoto(photo);
-                } catch (Exception e) {
                 }
+
+                if(img instanceof File) {
+                    imgPath = (File)img;
+                } else if(img instanceof URL) {
+                    URL url = (URL)img;
+
+                    String tempFile = TelegramBot.TEMP_DIR + "/" + Utilities.timeStamp();
+
+                    try(FileOutputStream fos = new FileOutputStream(tempFile)) {
+                        fos.write(Utilities.getURL(url.toString()));
+                    }
+
+                    imgPath = new File(tempFile);
+                }
+                else {
+                    return;
+                }
+
+
+                SendPhoto photo = new SendPhoto().setChatId(chatId).setNewPhoto(imgPath);
+
+                if(text != null) {
+                    photo.setCaption(text);
+                }
+
+                TelegramBot.this.sendPhoto(photo);
+            } catch (Exception e) {
             }
-        };
+        });
         
         thread.start();
     }
